2db426fe51fb2fb4da8b3323ea1f8b33bc8e6158
Saurabh Beniwal <saurabhbeniwal21062003@gmail.com>
2025-05-30 01:33:54 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -14,6 +14,9 @@ dotenv.config();
 connectDB();
 const app = express();
 
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './frontend/build')));
