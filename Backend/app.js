7cc2c777380fecc327176310b8ede104b265c421
Saurabh Beniwal <saurabhbeniwal21062003@gmail.com>
2025-06-01 01:15:18 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -33,7 +33,7 @@ app.use('/api/users', userRoutes);
 
 app.use(express.static(path.join(__dirname, './frontend/build')));
 
-app.get('*',function(req,res){
+app.use('*',function(req,res){
     res.sendFile(path.join(__dirname, './frontend/build/index.html'));
 });
 
